fd0c480844dfd2fd969e5141125c9df091fe3aa6
GoktugYmsk <gogtug033@outlook.com>
2023-08-30 17:30:33 +0300

    bugs are fixed

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -4,27 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 
 import { Feather, Ionicons } from '@expo/vector-icons';
 
-import Popup from '../popup';
-import searcImage from '../../assets/images.jpeg'
-
-function Statictic() {
-    return (
-        <View >
-            <Text>Bu sayfa istatistik sayfasıdır</Text>
-        </View>
-    );
-}
-
-function Home() {
-    return (
-        <View>
-            {/* Your home content */}
-        </View>
-    );
-}
 
 function Footer({ refresh, boot }) {
-    const [showPopup, setShowPopup] = useState(false);
 
     const navigation = useNavigation();
 
@@ -32,14 +13,11 @@ function Footer({ refresh, boot }) {
         refresh();
     };
 
-    const handleMagnifyingClick = () => {
-        setShowPopup(true);
-    };
 
     return (
         <View style={styles.lowerBar}>
             <View style={styles.nav}>
-                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <TouchableOpacity>
                     <View style={styles.iconBlok} >
                         <Feather name="home" size={25} color="white" />
                         <Text style={styles.text} >Anasayfa</Text>
@@ -51,24 +29,23 @@ function Footer({ refresh, boot }) {
                         <Text style={styles.text} >Yenile</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={handleMagnifyingClick}>
+                <TouchableOpacity >
                     <View style={styles.iconBlok} >
                         <Ionicons name='search' size={25} color='white' />
                         <Text style={styles.text} >Filterele</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Statictic')}>
+                <TouchableOpacity>
                     <View style={styles.iconBlok} >
                         <Ionicons name="ios-swap-horizontal" size={25} color="white" />
                         <Text style={styles.text} >İstatistik</Text>
                     </View>
                 </TouchableOpacity>
-                <View style={styles.iconBlok} >
+                <TouchableOpacity style={styles.iconBlok} >
                     <Ionicons name="md-map" size={25} color="white" />
                     <Text style={styles.text} >Harita</Text>
-                </View>
+                </TouchableOpacity>
             </View>
-            {showPopup && <Popup setShow={setShowPopup} />}
         </View>
     );
 }
